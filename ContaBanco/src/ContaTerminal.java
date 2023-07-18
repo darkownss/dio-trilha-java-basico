9861207e2b9b390f2088a56ce4a230d493faef32
Darkowns <propaltalt@gmail.com>
2023-07-18 16:25:44 -0300

    .

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,21 @@
+import java.util.*;
+
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
+       int numero = Integer.parseInt(args[0]);
+       String agencia = args[1];
+       String nomeCliente = args[2];
+       double saldo = Double.parseDouble(args[3]);
+       Scanner sc = new Scanner(System.in);
+
+
+       System.out.println("Por favor, digite o número da Agência !");
+        String str = sc.nextLine();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco,",nomeCliente);
+         System.out.println("sua agência é "+ agencia + ", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+
        
-
-
 
         
     }
